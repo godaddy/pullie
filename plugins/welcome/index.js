d040c38ef63712495ee72ce9bc565ed1868d949e
Shaun Lobisnger <Shaun.lobsinger@gmail.com>
2019-11-26 15:58:16 -0700

    fleshed out the plugin based on the other plugins.

--- plugins/welcome/index.js
+++ plugins/welcome/index.js
@@ -1,1 +1,36 @@
-// lets welcome our new friends here!
+const BasePlugin = require('../base');
+// const Commenter = require('../../commenter');
+
+class WelcomePlugin extends BasePlugin {
+  /**
+   * Whether this plugin processes edit actions
+   * @public
+   * @override
+   * @returns {Boolean} Whether this plugin processes edit actions
+   */
+  get processesEdits() {
+    return false;
+  }
+
+  /**
+   * @typedef {import('@octokit/webhooks').WebhookPayloadPullRequest} WebhookPayloadPullRequest
+   * @typedef {WebhookPayloadPullRequest & { changes: Object }} WebhookPayloadPullRequestWithChanges
+   * @typedef {import('probot').Context<WebhookPayloadPullRequestWithChanges>} ProbotContext
+   */
+  /**
+   * Process a PR and check to see if we have a new friend
+   *
+   * @memberof WelcomePlugin
+   * @public
+   * @override
+   * @param {ProbotContext} context webhook context
+   * @param {Commenter} commenter Commenter
+   * @param {Object} config Configuration for this plugin
+   * @param {String[]} config.files File paths to require in the PR
+   */
+  async processRequest(context, commenter, config) {
+    // do stuff here like a champ!
+  }
+}
+
+module.exports = WelcomePlugin;
